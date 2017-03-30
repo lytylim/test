f1c65121c5f4a0b07a9506da3aff44dfbfd5e252
lytylim <lyty.lim@dexcom.com>
2017-03-30 11:27:00 -0700

    Update pi.py

--- pi.py
+++ pi.py
@@ -32,15 +32,15 @@ if __name__ == "__main__":
         .appName("PythonPi")\
         .getOrCreate()
 
-    partitions = int(sys.argv[1]) if len(sys.argv) > 1 else 2
-    n = 100000 * partitions
+    #partitions = int(sys.argv[1]) if len(sys.argv) > 1 else 2
+    #n = 100000 * partitions
 
-    def f(_):
-        x = random() * 2 - 1
-        y = random() * 2 - 1
-        return 1 if x ** 2 + y ** 2 <= 1 else 0
+    #def f(_):
+    #    x = random() * 2 - 1
+    #    y = random() * 2 - 1
+    #    return 1 if x ** 2 + y ** 2 <= 1 else 0
 
-    count = spark.sparkContext.parallelize(range(1, n + 1), partitions).map(f).reduce(add)
-    print("Pi is roughly %f" % (4.0 * count / n))
+    #count = spark.sparkContext.parallelize(range(1, n + 1), partitions).map(f).reduce(add)
+    #print("Pi is roughly %f" % (4.0 * count / n))
     print (sys.argv[1])
     spark.stop()
